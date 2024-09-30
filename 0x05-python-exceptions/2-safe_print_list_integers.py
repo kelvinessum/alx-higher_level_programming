d07fb6bce84b40219d733dbfa63baee3c072962c
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-09-30 15:31:48 -0700

    python exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[count]), end="")
                 printed_count += 1
-            except (ValueError, TypeError):
+            except(ValueError, TypeError):
                 pass
             count += 1
     except IndexError:
