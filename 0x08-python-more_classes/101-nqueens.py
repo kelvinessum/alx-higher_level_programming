7d1d3a22773155a62e5e3a7ca19ecfaf8eed5379
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-10-09 05:28:10 -0700

    python

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Solves the N-queens puzzle"""
+
+import sys
+
+
+def print_solution(board):
+    """Print the board in the required format."""
+    solution = []
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col] == 1:
+                solution.append([row, col])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, col):
+    """Use backtracking to solve the N Queens problem."""
+    if col >= len(board):
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+
+    return res
+
+
+def nqueens(N):
+    """Set up the N Queens problem and solve it."""
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    if not solve_nqueens(board, 0):
+        print("No solution exists")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    nqueens(N)
