32f501b8a61b4ad7d3ca8d5a7773e46b68ae052e
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-10-15 10:26:38 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines a class BaseGeometry with an area method.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an Exception indicating the area method is not implemented.
+        """
+        raise Exception("area() is not implemented")
