deeda2518e81d9d02b4c3e66f97b994a03cd2058
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-10-15 18:41:24 -0700

    input output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a function to write a string to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of char.
+    Args:
+        filename (str): The name of the file.
+        text (str): The string to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
