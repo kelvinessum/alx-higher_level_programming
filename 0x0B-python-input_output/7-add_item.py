df899a4858e4597ce97cae560ca21d26a54eb7e7
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-10-15 19:16:05 -0700

    input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,6 @@ Script that adds all command-line arguments to a Python list
 and saves them to a file.
 """
 import sys
-form os import path
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -12,11 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-if path.exists(filename):
+try:
     my_list = load_from_json_file(filename)
-else:
+except FileNotFoundError:
     my_list = []
 
 my_list.extend(sys.argv[1:])
-
 save_to_json_file(my_list, filename)
